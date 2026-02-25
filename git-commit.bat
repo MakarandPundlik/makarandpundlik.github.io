c9258e885e46add7a59a459db8bed8c31e40feb5
MakarandPundlik <mmp2308@gmail.com>
2026-02-24 21:47:55 -0500

    Remove README and .nojekyll, keep workflow only

--- git-commit.bat
+++ git-commit.bat
@@ -1,4 +1,4 @@
 @echo off
 cd /d F:\Codes\new_portfolio
 git add -A
-git commit -m "Use branch deployment for Pages; remove Actions workflow"
+git commit -m "Remove README and .nojekyll, keep workflow only"
